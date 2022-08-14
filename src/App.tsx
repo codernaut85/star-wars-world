18b6a1c8faad837f3d6af39f54b18f96363ff214
Grant Sykes <grantrjsykes@googlemail.com>
2022-08-14 22:54:27 +0100

    Re-add /people route

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/people/" element={<Home />} />
         <Route path="/people/:id" element={<Person />} />
       </Routes>
     </BrowserRouter>
